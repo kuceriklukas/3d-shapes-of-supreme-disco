22f5e3e1668a58750cd0ed8bcf556782d5eaa54d
Lukas Kucerik <kuceriklukas@gmail.com>
2019-10-14 15:44:45 +0200

    Add all the cube files

---
+++ script.js
@@ -0,0 +1,23 @@
+function rotateRight() {
+    document.getElementById("cube").className = "";
+    document.getElementById("cube").className = "show-right";
+}
+
+function rotateLeft() {
+    document.getElementById("cube").className = "";
+    document.getElementById("cube").className = "show-left";
+}
+
+function rotateFront() {
+    document.getElementById("cube").className = "";
+}
+
+function rotateTop() {
+    document.getElementById("cube").className = "";
+    document.getElementById("cube").className = "show-top";
+}
+
+function rotateBottom() {
+    document.getElementById("cube").className = "";
+    document.getElementById("cube").className = "show-bottom";
+}
